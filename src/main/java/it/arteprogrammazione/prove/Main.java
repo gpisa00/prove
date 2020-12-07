10fc9d03dcd94376fbe66270d74aaab3743ca584
gpisa00 <giuseppepisa00@gmail.com>
2020-12-07 14:13:27 +0100

    test

--- src/main/java/it/arteprogrammazione/prove/Main.java
+++ src/main/java/it/arteprogrammazione/prove/Main.java
@@ -1,15 +1,9 @@
 package it.arteprogrammazione.prove;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
